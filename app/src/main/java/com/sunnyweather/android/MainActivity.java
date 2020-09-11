898dd372e950ee8a199d302f672ea1088860c0c4
Rookie425 <741348741@qq.com>
2020-09-11 16:36:28 +0800

    01_commit

--- app/src/main/java/com/sunnyweather/android/MainActivity.java
+++ app/src/main/java/com/sunnyweather/android/MainActivity.java
@@ -1,9 +1,12 @@
 package com.sunnyweather.android;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.os.Handler;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Handler handler = new Handler();
+        new Thread(){
+            @Override
+            public void run() {
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                Date date = new Date(System.currentTimeMillis());
+                String time = simpleDateFormat.format(date);
+            }
+        }.start();
     }
 }
