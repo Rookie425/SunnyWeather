ce159a7ed4aaa7ed851dfe5b92549075ae1af6e4
zhenghao <741348741@qq.com>
2020-09-26 11:30:43 +0800

    09261130

--- app/src/main/java/com/sunnyweather/android/logic/network/PlaceService.java
+++ app/src/main/java/com/sunnyweather/android/logic/network/PlaceService.java
@@ -6,9 +6,7 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
-public class PlaceService {
+public interface PlaceService {
     @GET("v2/place?token=${SunnyWeatherApplication.TOKEN}&lang=zh_CN")
-    public Call<PlaceResponse> searchPlaces(@Query("query") String query){
-        return null;
-    }
+    Call<PlaceResponse> searchPlaces(@Query("query") String query);
 }
---
+++ app/src/main/java/com/sunnyweather/android/logic/network/ServiceCreator.java
@@ -0,0 +1,13 @@
+package com.sunnyweather.android.logic.network;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ServiceCreator {
+    private final String BASE_URL = "https://api.caiyunapp.com";
+    private Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+
+}
